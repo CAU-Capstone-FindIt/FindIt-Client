e4ea01ef62aebb5d5308eb2c9a87112b8161a414
변현석 <bhs3066@gmail.com>
2024-10-06 18:21:06 +0900

    feat:develop브랜치 home.jsx변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,42 @@
-import React from 'react'
-import styled from 'styled-components'
-import IconButton from '@mui/material/IconButton';
-import RoofingIcon from '@mui/icons-material/Roofing';
-import Input from '@mui/material/Input';
-import { useState } from 'react';
+import React, { useState } from "react";
+import styled from "styled-components";
+import IconButton from "@mui/material/IconButton";
+import RoofingIcon from "@mui/icons-material/Roofing";
+import Nav from "./Nav";
+import Map from "./Map";
+import FloatingButtons from "./FloatingButtons";
 
 const Home = () => {
-  const [inputValue, setInputValue] = useState('');
+  const [reportMode, setReportMode] = useState(null);
+  const [markerVisible, setMarkerVisible] = useState(null);
+  const handleStartReport = (mode) => {
+    setReportMode(mode);
+    if (markerVisible) {
+      markerVisible(true); // 마커 표시
+    }
+  };
+  const [inputValue, setInputValue] = useState("");
 
-  const handleClick = () => {
-    console.log('hihi')
-    console.log(inputValue)
-  }
-
-  const inputChange = (event) => {
-    setInputValue(event.target.value);
-  }
+  const handleInfoInputClick = () => {
+    console.log("정보 입력 버튼 클릭됨");
+  };
 
   return (
     <Container>
-      <IconButton variant="text">
-        <RoofingIcon onClick = {handleClick}/>
-      </IconButton>
-      <Input placeholder="Placeholder" value={inputValue} onChange={inputChange} />
+      {/* <IconButton color="primary" variant="text">
+        <RoofingIcon onClick={handleClick} />
+      </IconButton> */}
+      <Map></Map>
+      <FloatingButtons onStartReport={handleStartReport} />
+      <Nav></Nav>
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const Container = styled.div`
-    background-color: gray;
-    width: 600px;
-    height: 900px;
-`
+  background-color: gray;
+  width: 600px;
+  height: 100%;
+`;
