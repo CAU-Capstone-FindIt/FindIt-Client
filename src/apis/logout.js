f18573423452ef5d708e103e01b270003275d5a2
kimtree24 <ktj503@naver.com>
2024-11-09 05:29:50 +0900

    FEAT:로그아웃 구현

---
+++ src/apis/logout.js
@@ -0,0 +1,20 @@
+export const handleLogout = async () => {
+    const token = localStorage.getItem("access")
+    //console.log(token)
+    localStorage.removeItem('access');
+    localStorage.removeItem('nickname');
+    localStorage.removeItem('profile');
+      alert('로그아웃 되었습니다.');
+    // try {
+    //   await axios.post('http://findit.p-e.kr:8080/api/users/logout', {}, {
+    //     headers: {
+    //       'Authorization': `Bearer ${token}`
+    //     }
+    //   });
+    //   localStorage.removeItem('token');
+    //   alert('로그아웃 되었습니다.');
+    //   // 사용자 리다이렉션 또는 UI 업데이트
+    // } catch (error) {
+    //   console.error("로그아웃 실패:", error);
+    // }
+  };
